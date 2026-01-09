bef2016ff259d55eb1d738a20af90e92feb442b8
Stanko K.R. <stanko@stanko.io>
2026-01-09 15:15:37 +0100

    Implement basic imports

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
     resources :exports, only: [ :create, :show ]
   end
 
+  resources :imports, only: %i[ new create show ]
+
   resources :users do
     scope module: :users do
       resource :avatar
@@ -164,6 +166,7 @@ Rails.application.routes.draw do
   end
 
   resource :landing
+
 
   namespace :my do
     resource :identity, only: :show
